ba2dac568f507534a064c550169df3adc38af136
Jerry Wang <jerrywangpico@gmail.com>
2021-03-19 01:31:12 -0400

    complete strategy 2 using three nested for loop

--- Homework3/derivative.jl
+++ Homework3/derivative.jl
@@ -8,14 +8,40 @@ function derivative_1(f, x, h)
     return df
 end
 
-function eval_function_derivative(f, fp, option, f_num, startx, endx, h)
+function derivative_2(f, r, n, pts)
+    k = 0:n
+    x_k = cos.(k .* (pi / n))
+    x_k = (r[2] + r[1]) / 2 .+ (r[2] - r[1]) / 2 .* x_k
+    y = zeros((pts,))
+    x = range(r[1], stop=r[2], length=pts)
+    w = ones(Float64, (n + 1, ))
+    f_k = f(x_k)
+    for ind_x in 1:pts
+        l_j = zeros((length(f_k),))
+        for j in 1:length(k)
+            for i in 1:length(k)
+                product = 1
+                for m in 1:length(k)
+                    if (m != j && m != i)
+                        product *= (x[ind_x] - x_k[m])  / (x_k[j] - x_k[m])
+                    end
+                end
+                if (i != j)
+                    l_j[j] += product / (x_k[j] - x_k[i])
+                end
+            end
+        end
+        y[ind_x] = sum(f_k .* l_j)
+    end
+    return y
+end
+
+function eval_function_derivative_1(f, fp, f_num, startx, endx, h)
     error_vec = zeros((3, length(h)))
     for i in range(1, stop=length(h))
         x = range(startx, stop=endx, length=10000)
         df_analytical = fp(x)
-        if option == 1
-            df = derivative_1(f, x, h[i])
-        end
+        df = derivative_1(f, x, h[i])
         error = abs.(df_analytical - df)
         error_vec[:, i] = [findmax(error)[1], median(error), findmin(error)[1]]
     end
@@ -36,25 +62,56 @@ function eval_function_derivative(f, fp, option, f_num, startx, endx, h)
     png(string("f",f_num,"_d",option,"_min.png"))
 end
 
+function eval_function_derivative_2(f, fp, f_num, startx, endx, n)
+    error_vec = zeros((3, length(n)))
+    x = range(startx, stop=endx, length=10000)
+    # df = nothing
+    for i in range(1, stop=length(n))
+        println(string("compute for n=", n[i]))
+        df_analytical = fp(x)
+        df = derivative_2(f, [startx, endx], n[i], 10000)
+        error = abs.(df_analytical - df)
+        error_vec[:, i] = [findmax(error)[1], median(error), findmin(error)[1]]
+    end
+    display(plot(n, error_vec[1, :],
+                title=string("max error of f", f_num),
+                xlabel="h",
+                ylabel="error"))
+    # png(string("f",f_num,"_d",option,"_max.png"))
+    display(plot(n, error_vec[2, :],
+                title=string("median error of f", f_num),
+                xlabel="h",
+                ylabel="error"))
+    # png(string("f",f_num,"_d",option,"_median.png"))
+    display(plot(n, error_vec[3, :],
+                title=string("min error of f", f_num),
+                xlabel="h",
+                ylabel="error"))
+    # png(string("f",f_num,"_d",option,"_min.png"))
+end
+
 h = range(1e-10, stop=0.1, length=100)
+n = 5:5:250
+# n = 10
 error_vec = zeros((3, length(h)))
 # Evaluate derivative of f(x)=e^x
 f(x) = exp.(x)
 fp(x) = exp.(x)
-eval_function_derivative(f, fp, 1, 1, 0, 5, h)
+# eval_function_derivative_1(f, fp, 1, 0, 5, h)
+eval_function_derivative_2(f, fp, 1, 0, 5, n)
 
-# Evaluate derivative of f(x)=1/(1+x^2)
-f(x) = 1 ./ (1 .+ x .^2)
-fp(x) = (2 .* x) ./ (1 .+ x .^ 2) .^ 2
-eval_function_derivative(f, fp, 1, 2, -5, 5, h)
-
-# Evaluate derivative of f(x)=e^{3x}sin(200x^2)/(1+20x^2)
-f(x) = exp.(3 .* x) .* sin.(200 .* x .^ 2) ./ (1 .+ 20 .* x .^ 2)
-fp1(x) = 3 .* exp.(3 .* x) .* sin.(200 .* x.^2) ./
-        (1 .+ 20 .* x .^ 2)
-fp2(x) = 40 .* exp.(3 .* x) .* x .* sin.(200 .* x .^2) ./
-        (1 .+ 20 .* x .^ 2).^2
-fp3(x) = 400 .* exp.(3 .* x) .* x .* cos.(200 .* x .^2) ./
-        (1 .+ 20 .* x .^ 2)
-fp(x) = fp1(x) - fp2(x) + fp3(x)
-eval_function_derivative(f, fp, 1, 3, 0, 1, h)
+# # Evaluate derivative of f(x)=1/(1+x^2)
+# f(x) = 1 ./ (1 .+ x .^2)
+# fp(x) = (2 .* x) ./ (1 .+ x .^ 2) .^ 2
+# eval_function_derivative_1(f, fp, 2, -5, 5, h)
+#
+# # Evaluate derivative of f(x)=e^{3x}sin(200x^2)/(1+20x^2)
+# f(x) = exp.(3 .* x) .* sin.(200 .* x .^ 2) ./ (1 .+ 20 .* x .^ 2)
+# fp1(x) = 3 .* exp.(3 .* x) .* sin.(200 .* x.^2) ./
+#         (1 .+ 20 .* x .^ 2)
+# fp2(x) = 40 .* exp.(3 .* x) .* x .* sin.(200 .* x .^2) ./
+#         (1 .+ 20 .* x .^ 2).^2
+# fp3(x) = 400 .* exp.(3 .* x) .* x .* cos.(200 .* x .^2) ./
+#         (1 .+ 20 .* x .^ 2)
+# fp(x) = fp1(x) - fp2(x) + fp3(x)
+# eval_function_derivative_1(f, fp, 3, 0, 1, h)
